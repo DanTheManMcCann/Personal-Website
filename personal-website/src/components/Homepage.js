ece45595c1f63b95bf69a5881c2ed640699ca853
Daniel MS <danielmccannsayles@gmail.com>
2021-11-20 22:27:25 -1000

    trying out tailwind

--- personal-website/src/components/Homepage.js
+++ personal-website/src/components/Homepage.js
@@ -14,11 +14,17 @@ class HomePage extends React.Component{
 
     render(){
         return( 
-            <>
+            <div class="bg-green-500 md:bg-red-500 lg:bg-green-500">
             <div>blah blah</div>
             <ButtonTrigger handleClick={this.handleClick}></ButtonTrigger>
             <ButtonTrigger handleClick={this.props.onClick}></ButtonTrigger>
-            </>
+            <div class="container flex mx-auto bg-white h-40 items-center justify-items-center">
+               <div class="bg-purple-400 text-white w-40" style={{boxShadow: '60px -16px teal',}}>
+                    <h1>This is header</h1>
+                    <p>This is the body of this area and hopefully it works</p>
+               </div>
+            </div>
+            </div>
         );
     }
 }
