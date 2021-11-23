e3f033b3f983f394892b024f0b18b375d5ca8c05
Daniel MS <danielmccannsayles@gmail.com>
2021-11-22 19:43:53 -1000

    Update Homepage.js
    more testing

--- personal-website/src/components/Homepage.js
+++ personal-website/src/components/Homepage.js
@@ -14,17 +14,28 @@ class HomePage extends React.Component{
 
     render(){
         return( 
+            <>
             <div class="bg-green-500 md:bg-red-500 lg:bg-green-500">
             <div>blah blah</div>
             <ButtonTrigger handleClick={this.handleClick}></ButtonTrigger>
             <ButtonTrigger handleClick={this.props.onClick}></ButtonTrigger>
-            <div class="container flex mx-auto bg-white h-40 items-center justify-items-center">
-               <div class="bg-purple-400 text-white w-40" style={{boxShadow: '60px -16px teal',}}>
+            </div>
+            <div class="bg-green-100 mx-10 flex bg-white h-40 items-center justify-evenly">
+               <div class="bg-purple-400 text-white w-40 rounded-lg" style={{boxShadow: '-30px -16px teal',}}>
+                    <h1>This is header</h1>
+                    <p>This is the body of this area and hopefully it works</p>
+               </div>
+               <div class="bg-purple-400 text-white w-40" style={{boxShadow: '-30px -16px teal',}}>
+                    <h1>This is header</h1>
+                    <p>This is the body of this area and hopefully it works</p>
+               </div>
+               <div class="bg-purple-400 text-white w-40" style={{boxShadow: '-30px -16px teal',}}>
                     <h1>This is header</h1>
                     <p>This is the body of this area and hopefully it works</p>
                </div>
             </div>
-            </div>
+            </>
+            
         );
     }
 }
